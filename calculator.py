70ac67f7657a5e46b566e54a419db5b4b62cee56
Victor A Medina <vmedina1014@gmail.com>
2019-12-15 20:19:30 -0800

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,57 +1,77 @@
+#Victor Medina
+#Assignemnt 4
+def empty():
+    page = """
+<h1>Calculator</h1>
+This calculator is able to run the following operands:
+<ul>
+    <li>add</li>
+        <ul>
+            <li>Example: Navigate to http://localhost:8080/add/5/10 to view the result of 5 + 10</li>
+        </ul>
+    <li>subtract</li>
+        <ul>
+            <li>Example: Navigate to http://localhost:8080/subtract/12/10 to view the result of 12 - 10</li>
+        </ul>
+    <li>multiply</li>
+        <ul>
+            <li>Example: Navigate to http://localhost:8080/multiply/35/16 to view the result of 35 x 16</li>
+        </ul>
+    <li>divide</li>
+        <ul>
+            <li>Example: Navigate to http://localhost:8080/divide/20/4 to view the result of 20 / 4</li>
+        </ul>
+</ul>
 """
-For your homework this week, you'll be creating a wsgi application of
-your own.
-
-You'll create an online calculator that can perform several operations.
-
-You'll need to support:
-
-  * Addition
-  * Subtractions
-  * Multiplication
-  * Division
-
-Your users should be able to send appropriate requests and get back
-proper responses. For example, if I open a browser to your wsgi
-application at `http://localhost:8080/multiple/3/5' then the response
-body in my browser should be `15`.
-
-Consider the following URL/Response body pairs as tests:
-
-```
-  http://localhost:8080/multiply/3/5   => 15
-  http://localhost:8080/add/23/42      => 65
-  http://localhost:8080/subtract/23/42 => -19
-  http://localhost:8080/divide/22/11   => 2
-  http://localhost:8080/               => <html>Here's how to use this page...</html>
-```
-
-To submit your homework:
-
-  * Fork this repository (Session03).
-  * Edit this file to meet the homework requirements.
-  * Your script should be runnable using `$ python calculator.py`
-  * When the script is running, I should be able to view your
-    application in my browser.
-  * I should also be able to see a home page (http://localhost:8080/)
-    that explains how to perform calculations.
-  * Commit and push your changes to your fork.
-  * Submit a link to your Session03 fork repository!
-
-
-"""
-
+    return page
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
 
-    return sum
+    sum = 0
+    for arg in args:
+        sum += int(arg)
+    return str(sum)
+    
 
-# TODO: Add functions for handling more arithmetic operations.
+def subtract(*args):
+    """ Returns a STRING with the difference of the arguments """
+
+    diff = 0
+    first = True
+    for arg in args:
+        if first:
+            diff = int(arg)
+            first = False
+        else:
+            diff = diff - int(arg)
+    return str(diff)
+    
+
+def multiply(*args):
+    """ Returns a STRING with the product of the arguments """
+
+    prod = 1
+    for arg in args:
+        prod *= int(arg)
+    return str(prod)
+    
+    
+def divide(*args):
+    """ Returns a STRING with the quotient of the arguments """
+
+    quot = 1
+    first = True
+    for arg in args:
+        if first:
+            quot = int(arg)
+            first = False
+        else:
+            quot /= int(arg)
+    return str(quot)
+
+
 
 def resolve_path(path):
     """
@@ -59,26 +79,51 @@ def resolve_path(path):
     arguments.
     """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    funcs = {
+        '': empty,
+        'add': add,
+        'subtract': subtract,
+        'multiply': multiply,
+        'divide': divide
+    }
+
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
+
 
     return func, args
 
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+
+    headers = [("Content-type", "text/html")]
+    try:
+        path = environ.get('PATH_INFO', None)
+        if path is None:
+            raise NameError
+        func, args = resolve_path(path)
+        body = func(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = "<h1>Not Found</h1>"
+    except Exception:
+        status = "500 Internal Server Error"
+        body = "<h1>Internal Server Error</h1>"
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode('utf8')]
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
