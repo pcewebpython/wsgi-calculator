65d8460e1627b11234cc7f258c0a99b8e752c977
Anthony McKeever <snip3rm00n@gmail.com>
2020-03-27 19:52:12 -0700

    Add logic for calculating numbers

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import traceback
+
 """
 For your homework this week, you'll be creating a wsgi application of
 your own.
@@ -41,19 +43,74 @@ To submit your homework:
 
 """
 
+def tagged(tag, content, end="\n", **kwargs):
+    # Turn kwargs into a collection of usable HTML element attributes.
+    # Or leave the attributes empty if nothing is present.
+    attrs = " ".join([f"{key}={value}" for key, value in kwargs.items()])
+    attrs = f" {attrs}" if len(attrs) > 0 else ""
+
+    return f"<{tag}{attrs}>{content}</{tag}>{end}"
+
 
 def add(*args):
     """ Returns a STRING with the sum of the arguments """
+    total = sum([int(i) for i in args])
+    return tagged("h1", f"Your total is: {total}")
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = "0"
 
-    return sum
+def subtract(*args):
+    start = int(args[0])
+    to_sub = args[1:]
 
-# TODO: Add functions for handling more arithmetic operations.
+    for number in to_sub:
+        start -= int(number)
 
-def resolve_path(path):
+    return tagged("h1", f"Your total is: {start}")
+
+
+def multiply(*args):
+    start = int(args[0])
+    to_mult = args[1:]
+
+    for number in to_mult:
+        start *= int(number)
+
+    return tagged("h1", f"Your total is: {start}")
+
+
+def divide(*args):
+    start = int(args[0])
+    to_divide = args[1:]
+
+    for number in to_divide:
+        start /= int(number)
+
+    return tagged("h1", f"Your total is: {start}")
+
+
+def index(*args):
+    title = tagged("h1", "WSGI Calculator")
+    detail = tagged("p", str("This website will help you calculate numbers.  "
+                             "Simply navigate to the operation you want to "
+                             "perform and the numbers you want to calculate "
+                             "against.  Try one of the examples below."))
+    example_text = tagged("h3", "Examples:")
+    table = []
+    examples = {"add": f"{args[0]}/add/4/5",
+                "subtract": f"{args[0]}/subtract/-7/10",
+                "multiply": f"{args[0]}/multiply/2/7",
+                "divide": f"{args[0]}/divide/20/5"}
+
+    for key, value in examples.items():
+        anchor = tagged("a", value, end="", href=value)
+        row = tagged("th", key, end="") + tagged("td", anchor, end="")
+        table.append(tagged("tr", row))
+    
+    table = tagged("table", "".join(table))
+    return "".join([title, detail, example_text, table])
+
+
+def resolve_path(path, environ_host):
     """
     Should return two values: a callable and an iterable of
     arguments.
@@ -63,22 +120,52 @@ def resolve_path(path):
     # examples provide the correct *syntax*, but you should
     # determine the actual values of func and args using the
     # path.
-    func = add
-    args = ['25', '32']
+    functions = {"add": add,
+                 "subtract": subtract,
+                 "multiply": multiply,
+                 "divide": divide}
+
+    if path == "/":
+        func = index
+        args = [environ_host]
+    else:
+        path = path.strip('/').split('/')
+        name = path[0]
+        args = path[1:]
+
+        func = functions.get(name)
+
+        if not func:
+            raise NameError
 
     return func, args
 
+
 def application(environ, start_response):
-    # TODO: Your application code from the book database
-    # work here as well! Remember that your application must
-    # invoke start_response(status, headers) and also return
-    # the body of the response in BYTE encoding.
-    #
-    # TODO (bonus): Add error handling for a user attempting
-    # to divide by zero.
-    pass
+    headers = [('Content-type', 'text/html')]
+    try:
+        path = environ.get("PATH_INFO", None)
+        if not path:
+            raise NameError
+        function, args = resolve_path(path, environ["HTTP_HOST"])
+        body = function(*args)
+        status = "200 OK"
+    except NameError:
+        status = "404 Not Found"
+        body = tagged("h1", "Not Found")
+    except ZeroDivisionError:
+        status = "400 Bad Request"
+        body = tagged("h1", "Cannot Divide By Zero")
+    except Exception:
+        status = "500 Internal Server Error"
+        body = tagged("h1", "Internal Server Error")
+        print(traceback.format_exc())
+    finally:
+        headers.append(('Content-length', str(len(body))))
+        start_response(status, headers)
+        return [body.encode("utf8")]
 
 if __name__ == '__main__':
-    # TODO: Insert the same boilerplate wsgiref simple
-    # server creation that you used in the book database.
-    pass
+    from wsgiref.simple_server import make_server
+    srv = make_server('localhost', 8080, application)
+    srv.serve_forever()
