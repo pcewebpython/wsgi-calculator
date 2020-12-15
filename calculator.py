0562fb845be1a1870226fe49d265c8b71be54d7f
stellie <kims82@gmail.com>
2020-12-15 15:32:29 -0800

    add functions for different operations, update resolve path function with operation functions

--- calculator.py
+++ calculator.py
@@ -1,16 +1,4 @@
 """
-For your homework this week, you'll be creating a wsgi application of
-your own.
-
-You'll create an online calculator that can perform several operations.
-
-You'll need to support:
-
-  * Addition
-  * Subtractions
-  * Multiplication
-  * Division
-
 Your users should be able to send appropriate requests and get back
 proper responses. For example, if I open a browser to your wsgi
 application at `http://localhost:8080/multiple/3/5' then the response
@@ -26,19 +14,6 @@ Consider the following URL/Response body pairs as tests:
   http://localhost:8080/               => <html>Here's how to use this page...</html>
 ```
 
-To submit your homework:
-
-  * Fork this repository (Session03).
-  * Edit this file to meet the homework requirements.
-  * Your script should be runnable using `$ python calculator.py`
-  * When the script is running, I should be able to view your
-    application in my browser.
-  * I should also be able to see a home page (http://localhost:8080/)
-    that explains how to perform calculations.
-  * Commit and push your changes to your fork.
-  * Submit a link to your Session03 fork repository!
-
-
 """
 
 # Stella Kim
@@ -47,16 +22,45 @@ To submit your homework:
 import traceback
 
 
+def home(*args):
+    """Returns homepage that explains how to perform calculations"""
+    return 'this is the homepage'
+
+
 def add(*args):
-    """ Returns a STRING with the sum of the arguments """
+    """Returns a STRING with the sum of the arguments"""
 
-    # TODO: Fill sum with the correct value, based on the
-    # args provided.
-    sum = '0'
+    result = str(int(args[0]) + int(args[1]))
+    body = 'Your total is: {}'.format(result)
 
-    return sum
+    return body
 
-# TODO: Add functions for handling more arithmetic operations.
+
+def subtract(*args):
+    """Returns a STRING with the difference of the arguments"""
+
+    result = str(int(args[0]) - int(args[1]))
+    body = 'Your total is: {}'.format(result)
+
+    return body
+
+
+def multiply(*args):
+    """Returns a STRING with the product of the arguments"""
+
+    result = str(int(args[0]) * int(args[1]))
+    body = 'Your total is: {}'.format(result)
+
+    return body
+
+
+def divide(*args):
+    """Returns a STRING with the quotient of the arguments"""
+
+    result = str(int(args[0]) / int(args[1]))
+    body = 'Your total is: {}'.format(result)
+
+    return body
 
 
 def resolve_path(path):
@@ -65,12 +69,23 @@ def resolve_path(path):
     arguments.
     """
 
-    # TODO: Provide correct values for func and args. The
-    # examples provide the correct *syntax*, but you should
-    # determine the actual values of func and args using the
-    # path.
-    func = add
-    args = ['25', '32']
+    funcs = {
+        '': home,
+        'add': add,
+        'subtract': subtract,
+        'multiply': multiply,
+        'divide': divide
+    }
+
+    path = path.strip('/').split('/')
+
+    func_name = path[0]
+    args = path[1:]
+
+    try:
+        func = funcs[func_name]
+    except KeyError:
+        raise NameError
 
     return func, args
 
@@ -98,6 +113,7 @@ def application(environ, start_response):
 
     # TODO (bonus): Add error handling for a user attempting
     # to divide by zero.
+
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
